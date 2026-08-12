8ef6811de8d1215fdc32fa9189b239c5c6fa0c0c
Nitesh Sharma <nirmit.s2005@gmail.com>
2026-08-12 18:57:54 +0530

    last upadte

--- script.js
+++ script.js
@@ -279,7 +279,7 @@
     setLoading(true);
 
     try {
-      const res = await fetch(API_URL, {
+      const res = await fetch(`${API_URL}/predict`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody),
